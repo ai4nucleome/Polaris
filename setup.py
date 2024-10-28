6ee5717bfbd85a9b7bd77174a37ceb530325d7c6
lab server yusen <lab_yusen@email.com>
2024-10-28 16:14:54 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,18 @@ A Unified Axial-aware Framework for Chromatin Loop Annotation in Bulk and Single
 """
 
 from setuptools import setup, find_packages
+with open("README.md","r") as readme:
+    long_des=readme.read()
 
 setup(
     name='polaris',
     version='1.0.0',
+    author="Yusen HOU, Yanlin Zhang",
+    author_email="yhou925@connect.hkust-gz.edu.cn",
+    description="A Unified Axial-aware Framework for Chromatin Loop Annotation in Bulk and Single-cell Hi-C Data",
+    long_description=long_des,
+    long_description_content_type="text/markdown",
+    url="https://github.com/compbiodsa/Polaris",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -19,6 +27,13 @@ setup(
             'polaris = polaris.polaris:cli',
         ],
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        # "License :: OSI Approved :: MIT License",
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Computational Biology',
+    ],
+    python_requires='>=3.9',
 )
 
 
